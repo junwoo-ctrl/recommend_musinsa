72948b6fbff4742efae0366eae2f36d7e23058af
mrx-junwoo <rlagud67@gmail.com>
2021-05-30 20:34:45 +0900

    default implement environment tools. \ 1. EnvTool: generate dev or prod environment name. 2. Configs: define two seperated configuration data types.

---
+++ src/app/util/tools.py
@@ -0,0 +1,51 @@
+import os
+from os import path
+from dataclasses import dataclass
+
+
+base_dir = path.dirname(path.dirname(path.dirname(path.abspath(__file__))))
+
+
+class EnvTool:
+    ENV_NAME_KEY: str = 'ENV'
+    ENV_DEV: str = 'DEV'
+    ENV_PROD: str = 'PROD'
+
+    @staticmethod
+    def get_env():
+        env = os.environ.get(EnvTool.ENV_NAME_KEY, EnvTool.ENV_DEV).lower()
+        return env
+
+
+@dataclass
+class BasicConfig:
+    BASE_PATH: str = base_dir
+    DB_POOL_RECYCLE: int = 900
+    DB_ECHO: bool = True
+
+
+@dataclass
+class DevConfig(BasicConfig):
+    PROJ_RELOAD: bool = True
+    user: str = 'musinsa'
+    password: str = 'xhakxhsoaql1!'
+    host: str = 'localhost'
+    database: str = 'dev'
+
+
+@dataclass
+class ProdConfig(BasicConfig):
+    PROJ_RELOAD: bool = False
+    user: str = 'musinsa'
+    password: str = 'xhakxhsoaql1!'
+    host: str = 'localhost'
+    database: str = 'prod'
+
+
+class Config:
+    
+    @staticmethod
+    def get_config():
+        config = dict(prod=ProdConfig(), dev=DevConfig())
+        env = EnvTool.get_env()
+        return config.get(env)
