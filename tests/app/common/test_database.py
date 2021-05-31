22137e5739cc7d5d7b848146939533134f363da2
mrx-junwoo <rlagud67@gmail.com>
2021-06-01 08:25:43 +0900

    change test dirname.

---
+++ tests/app/common/test_database.py
@@ -0,0 +1,30 @@
+
+from dataset import Database as database_obj
+
+from src.app.common.database import Database
+
+
+def test_database_lifecycle():
+    
+    dbw = Database()
+    
+    # initlialize properties
+    assert dbw.config
+    assert dbw.url
+    assert db2.db
+
+    # get_db()
+    db_obj = dbw.get_db()
+    assert isinstance(db_obj, database_obj)
+
+    # _pint()
+    expected = True
+    actual = db._ping()
+    assert expected == actual
+
+    # query()
+    expected = True
+
+    statement = 'select 1'
+    acutal = db.query(statement)
+    assert expected == actual
---
+++ tests/core/v1/test_similar_keyword_recommender.py
@@ -0,0 +1,31 @@
+from datetime import datetime
+
+from src.core.v1.similar_keyword_recommender import SimilarKeywordRecommender
+from src.app.common.data import RecommendWord, RequestSearchWord, RecommendResult
+
+
+def test_search_similar_keyword():
+    mock_request_search_word = RequestSearchWord(
+        request_date=datetime.now(),
+        purpose='search similar',
+        message='ASAP',
+        word='nike',
+        num_limit=5,
+        request_from='musinsa',
+    )
+
+    similar_keyword_recommender = SimilarKeywordRecommender()
+    actual_recommend_list = similar_keyword_recommender.search(mock_request_search_word)
+    
+    assert isinstance(actual_recommend_list, list)
+    assert isinstance(actual_recommend_list[0], RecommendWord)
+
+
+def test_convert_recommend_result():
+    mock_recommend_result = RecommendResult(
+        findby="findby infix pattern",
+        word="isnikegood",
+    )
+
+    actual_recommend_word = SimilarKeywordRecommender._convert_recommend_result(mock_recommend_result)
+    assert isinstance(actual_recommend_word, RecommendWord)
