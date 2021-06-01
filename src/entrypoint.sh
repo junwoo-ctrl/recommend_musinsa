520b095c50e9eff9b5abe0ecaeabc6a1233bd9eb
mrx-junwoo <rlagud67@gmail.com>
2021-06-01 09:25:33 +0900

    fix python command.

--- src/entrypoint.sh
+++ src/entrypoint.sh
@@ -1,10 +1,10 @@
 case "$1" in
   test)
-    exec python -m pytest -s tests
+    exec python3 -m pytest -s tests
     ;;
   setup)
-	exec python /src/app/init/create_mysql_db.py init-db-scheme
-	exec python /src/app/init/create_mysql_db.py upload-data
+	exec python3 /src/app/init/create_mysql_db.py init-db-scheme
+	exec python3 /src/app/init/create_mysql_db.py upload-data
 	;;
   *)
     exec "$@"
