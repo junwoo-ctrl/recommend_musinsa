49586cc5caa8e8f1d7e0a4d99aafc903bb2a6c44
mrx-junwoo <rlagud67@gmail.com>
2021-06-01 16:47:31 +0900

    test code setup

--- src/entrypoint.sh
+++ src/entrypoint.sh
@@ -1,7 +1,7 @@
 case "$1" in
   test)
-	exec python3 /src/server/init/create_mysql_db.py init-db-scheme
-	exec python3 /src/server/init/create_mysql_db.py upload-data
+	#exec python3 /src/server/init/create_mysql_db.py init-db-scheme
+	#exec python3 /src/server/init/create_mysql_db.py upload-data
     exec python3 -m pytest -s tests
     ;;
   setup)
