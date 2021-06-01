98426be9c05cb3ad303441c3e655baae44613607
mrx-junwoo <rlagud67@gmail.com>
2021-06-01 17:05:12 +0900

    fix path2.

--- src/entrypoint.sh
+++ src/entrypoint.sh
@@ -5,8 +5,8 @@ case "$1" in
     exec python3 -m pytest -s tests
     ;;
   setup)
-	exec python3 /src/server/init/create_mysql_db.py init-db-scheme
-	exec python3 /src/server/init/create_mysql_db.py upload-data
+	exec python3 /server/init/create_mysql_db.py init-db-scheme
+	exec python3 /server/init/create_mysql_db.py upload-data
 	;;
   *)
     exec "$@"
